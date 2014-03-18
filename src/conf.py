44a240f68ac55cb7d1963b92f76cdbacb31c0159
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-03-18 20:48:11 +0100

    Hell HTML

--- src/conf.py
+++ src/conf.py
@@ -515,29 +515,6 @@ SOCIAL_BUTTONS_CODE = """
 
 # Local Search with tipue
 
-SEARCH_FORM = """
-<span class="navbar-form pull-left">
-<input type="text" id="tipue_search_input">
-</span>"""
-
-BODY_END = """
-<script type="text/javascript" src="/assets/js/tipuesearch_set.js"></script>
-<script type="text/javascript" src="/assets/js/tipuesearch.js"></script>
-<script type="text/javascript">
-$(document).ready(function() {
-  $('#tipue_search_input').tipuesearch({
-      'mode': 'json',
-      'contentLocation': '/assets/js/tipuesearch_content.json',
-      'showUrl': false
-  });
-});
-</script>
-"""
-
-EXTRA_HEAD_DATA = """
-<link rel="stylesheet" type="text/css" href="/assets/css/tipuesearch.css">
-<div id="tipue_search_content" style="margin-left: auto; margin-right: auto; padding: 20px;"></div>
-"""
 ENABLED_EXTRAS = ['local_search']
 
 # Use content distribution networks for jquery and twitter-bootstrap css and js
